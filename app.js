04b0da2a20f75c3ddccb3ea0c01a19333b584df5
JoaoVitor <123565897+JoaoVitorFerreiraS2@users.noreply.github.com>
2023-03-29 10:52:49 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
+falhasConsecutivas = 0;
+
 function verificarStatus() {
-
-    const statusBtn = document.getElementById('status-btn');
-    statusBtn.style.backgroundColor = 'gray';
-
-    setInterval(() => {
-    fetch('http://127.0.0.1:8080/')
+  const statusBtn = document.getElementById('status-btn');
+  statusBtn.style.backgroundColor = 'gray';
+  fetch('http://127.0.0.1:8080/')
     .then(response => response.text())
     .then(text => {
       console.log(text);
       if (text.includes('online')) {
+        falhasConsecutivas = 0; // reinicia a contagem de falhas consecutivas
         statusBtn.style.backgroundColor = 'green';
       } else {
-        statusBtn.style.backgroundColor = 'red';
+        falhasConsecutivas++;
+        if (falhasConsecutivas >= 10) { 
+          statusBtn.style.backgroundColor = 'red';
+        } else if (falhasConsecutivas >= 5) { 
+          statusBtn.style.backgroundColor = 'yellow';
+        } 
       }
     })
-
-      .catch(error => console.error(error));
-  }, 4000);
+    .catch(error => console.error(error));
 }
+
+// faz a primeira verificação ao carregar a página
+verificarStatus();
+
+// define a verificação automática a cada 4 segundos
+setInterval(verificarStatus, 4000);
